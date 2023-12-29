b5599651b7441e362bc4bc13ed91b8e36786f926
pch980330 <pch980330@gmail.com>
2023-12-29 12:56:25 +0900

    포트폴리오 완성

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 window.onload = function(){
+    AOS.init();
     // 스크롤 내리면 header에 active 추가
     let header = document.querySelector(".header")
     window.addEventListener("scroll",function(){
@@ -6,6 +7,21 @@ window.onload = function(){
             header.classList.add("active")
         }else{
             header.classList.remove("active")
+        }
+    })
+
+    // 모바일 메뉴 버튼
+    let moMenuBtn = document.querySelector(".mo-menu-btn")
+    let sideMenu = document.querySelector(".side-menu")
+    moMenuBtn.addEventListener("click", function(){
+        sideMenu.classList.toggle("active")
+        this.classList.toggle("active")
+    })
+    window.addEventListener("resize",function(){
+        let winWidth = window.innerWidth
+        if(winWidth > 768){
+            sideMenu.classList.remove("active")
+            moMenuBtn.classList.remove("active")
         }
     })
 
